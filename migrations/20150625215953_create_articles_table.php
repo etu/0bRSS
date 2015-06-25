4fbf6045ba8b183e8f4e98473729b2d229a4ad37
Elis Axelsson <elis.axelsson@gmail.com>
2015-06-26 00:31:46 +0200

    Added migration to create articles table

---
+++ migrations/20150625215953_create_articles_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateArticlesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $articles = $this->table('articles');
+        $articles->addColumn('title', 'string', ['limit' => 64])
+                 ->addColumn('feedId', 'integer')
+                 ->addColumn('body', 'text')
+                 ->addColumn('uri', 'string', ['limit' => 256])
+                 ->addColumn('published', 'datetime')
+                 ->addIndex(['title', 'uri', 'published'])
+                 ->addForeignKey('feedId', 'feeds', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
+                 ->create();
+    }
+}
