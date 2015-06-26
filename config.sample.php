803640a9dc88d19997f2112fb6bc6f9cb5a686c2
Elis Axelsson <elis.axelsson@gmail.com>
2015-06-26 23:16:59 +0200

    Added sample php-config

---
+++ config.sample.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'paths' => [
+        'migrations' => __DIR__.'/migrations'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database' => 'database',
+
+        'database' => [
+            'adapter' => 'pgsql',       // mysql, pgsql
+            'host'    => 'localhost',   // Database hostname
+            'name'    => '0bRSS_dev',   // Database name
+            'user'    => '0bRSS',       // Database username
+            'pass'    => 'secret-pass', // Database password
+            'port'    => 5432,          // Database port: 5432 for pgsql and 3306 for mysql
+            'charset' => 'utf8'         // Charset, don't change this
+        ]
+    ]
+];
