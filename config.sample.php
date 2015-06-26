3f5994cfea353ba441401de3214cb7d6fd310a06
Elis Axelsson <elis.axelsson@gmail.com>
2015-06-26 23:18:20 +0200

    Changed example database name

--- config.sample.php
+++ config.sample.php
@@ -11,7 +11,7 @@ return [
         'database' => [
             'adapter' => 'pgsql',       // mysql, pgsql
             'host'    => 'localhost',   // Database hostname
-            'name'    => '0bRSS_dev',   // Database name
+            'name'    => '0bRSS',       // Database name
             'user'    => '0bRSS',       // Database username
             'pass'    => 'secret-pass', // Database password
             'port'    => 5432,          // Database port: 5432 for pgsql and 3306 for mysql
