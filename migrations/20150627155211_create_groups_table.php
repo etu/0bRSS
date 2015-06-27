db3c0c9f6a8c30c544c7b451829e7505bb1790ee
Elis Axelsson <elis.axelsson@gmail.com>
2015-06-27 18:23:17 +0200

    Added migration for a groups table

---
+++ migrations/20150627155211_create_groups_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateGroupsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $groups = $this->table('groups');
+        $groups->addColumn('name',  'string', ['limit' => 64])
+               ->addColumn('system', 'boolean')
+
+               ->addIndex(['name'], ['unique' => true])
+
+               ->create();
+    }
+}
