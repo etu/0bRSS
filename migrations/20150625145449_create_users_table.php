dd79531454ab45039515b4497a33a11d3d0d5e10
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-01 11:58:16 +0200

    Changed length of varchars

--- migrations/20150625145449_create_users_table.php
+++ migrations/20150625145449_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends AbstractMigration
     public function change()
     {
         $users = $this->table('users');
-        $users->addColumn('email',      'string',   ['limit'   => 64])
-              ->addColumn('password',   'string',   ['limit'   => 64])
-              ->addColumn('name',       'string',   ['limit'   => 64])
+        $users->addColumn('email',      'string',   ['limit'   => 255])
+              ->addColumn('password',   'string',   ['limit'   => 255])
+              ->addColumn('name',       'string',   ['limit'   => 255])
               ->addColumn('created',    'datetime', ['default' => 'CURRENT_TIMESTAMP'])
               ->addColumn('updated',    'datetime', ['update'  => 'CURRENT_TIMESTAMP', 'null' => true])
               ->addColumn('last_login', 'datetime', ['update'  => 'CURRENT_TIMESTAMP', 'null' => true])
