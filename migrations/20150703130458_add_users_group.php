2eb67ba995b8a768f51c58587215af76c84a2e9e
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-03 15:10:37 +0200

    Added migration to add users group to the groups table

---
+++ migrations/20150703130458_add_users_group.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddUsersGroup extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+    public function change()
+    {
+    }
+    */
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->execute("INSERT INTO groups (name, system) VALUES ('users', true)");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
