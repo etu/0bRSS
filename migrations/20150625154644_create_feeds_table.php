4427d80b7d375d589f473de5b0778aaea1e126d1
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-04 14:39:23 +0200

    Casing

--- migrations/20150625154644_create_feeds_table.php
+++ migrations/20150625154644_create_feeds_table.php
@@ -16,8 +16,8 @@ class CreateFeedsTable extends AbstractMigration
     {
         $feeds = $this->table('feeds');
         $feeds->addColumn('name',            'string',   ['limit'   => 64])
-              ->addColumn('websiteUri',      'string',   ['limit'   => 255])
-              ->addColumn('feedUri',         'string',   ['limit'   => 255])
+              ->addColumn('website_uri',     'string',   ['limit'   => 255])
+              ->addColumn('feed_uri',        'string',   ['limit'   => 255])
               ->addColumn('added',           'datetime', ['default' => 'CURRENT_TIMESTAMP'])
               ->addColumn('updated',         'datetime', ['update'  => 'CURRENT_TIMESTAMP', 'null' => true])
               ->addColumn('update_interval', 'integer',  ['signed'  => false])
