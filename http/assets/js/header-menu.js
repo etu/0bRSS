824e70458d2eff6c8da3e77d3ec17ed6b8f20684
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-10 23:31:32 +0200

    Added a dropdown menu

---
+++ http/assets/js/header-menu.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Open and close button for menu
+ */
+$('header-menu-button').addEvent('click', function (event) {
+    $('header-menu').setStyle(
+        'display',
+        $('header-menu').getStyle('display') === 'none' ? 'block' : 'none'
+    );
+});
+
+/**
+ * Catch all clicks on the html element (everything basicly),
+ * then filter out which is not interesting (menu button and menu itself)
+ * and then close the menu if it's not catched in the not interesting filter
+ */
+$$('html')[0].addEvent('click', function (event) {
+    if (event.target                           === $('header-menu-button') ||
+        event.target.getParent('#header-menu') === $('header-menu') ||
+        event.target                           === $('header-menu')) {
+        return false;
+    }
+
+    if ('block' === $('header-menu').getStyle('display')) {
+        $('header-menu').setStyle('display', 'none');
+    }
+});
