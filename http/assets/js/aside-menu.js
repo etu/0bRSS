d86aa32baa251d15cbc1a9507488787c82f80a6f
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-11 11:05:16 +0200

    Hide aside-button on loginpage, and don't run the JS if the elements doesn't exist

--- http/assets/js/aside-menu.js
+++ http/assets/js/aside-menu.js
@@ -1,60 +1,60 @@
 'use strict';
 
-
-
-/**
- * Change aside margin to show it
- * Change content width do adjust to fit on the side of aside
- */
-var showAside = (function() {
-    $('aside-menu').setStyle('margin-left', '0rem');
-    $('content').setStyle('width', 'calc(100% - 17rem)');
-});
-
-
-
-/**
- * Change aside margin to hide it
- * Change content width do adjust to fit on the side of aside
- */
-var hideAside = (function() {
-    $('aside-menu').setStyle('margin-left', '-18rem');
-    $('content').setStyle('width', '100%');
-});
+if ($('show-aside-button')) {
+    /**
+     * Change aside margin to show it
+     * Change content width do adjust to fit on the side of aside
+     */
+    var showAside = (function() {
+        $('aside-menu').setStyle('margin-left', '0rem');
+        $('content').setStyle('width', 'calc(100% - 17rem)');
+    });
 
 
 
-/**
- * Open and close button for aside-menu
- */
-$('show-aside-button').addEvent('click', function (event) {
-    // Get the aside current margin-left as integer
-    var asideMargin = parseInt($('aside-menu').getStyle('margin-left'));
-
-    if (0 === asideMargin) {
-        hideAside();
-
-        return true;
-    }
-
-    showAside();
-});
+    /**
+     * Change aside margin to hide it
+     * Change content width do adjust to fit on the side of aside
+     */
+    var hideAside = (function() {
+        $('aside-menu').setStyle('margin-left', '-18rem');
+        $('content').setStyle('width', '100%');
+    });
 
 
 
-/**
- * Catch all clicks on the content element, then check if we should close the
- * aside or not.
- */
-$('content').addEvent('click', function (event) {
-    var asideButtonVisible = $('show-aside-button').getStyle('display') !== 'none';
-    var asideVisible = parseInt($('aside-menu').getStyle('margin-left')) === 0;
+    /**
+     * Open and close button for aside-menu
+     */
+    $('show-aside-button').addEvent('click', function (event) {
+        // Get the aside current margin-left as integer
+        var asideMargin = parseInt($('aside-menu').getStyle('margin-left'));
 
-    if (false === asideButtonVisible && false === asideVisible) {
+        if (0 === asideMargin) {
+            hideAside();
+
+            return true;
+        }
+
         showAside();
-    }
+    });
 
-    if (true === asideButtonVisible && true === asideVisible) {
-        hideAside();
-    }
-});
+
+
+    /**
+     * Catch all clicks on the content element, then check if we should close the
+     * aside or not.
+     */
+    $('content').addEvent('click', function (event) {
+        var asideButtonVisible = $('show-aside-button').getStyle('display') !== 'none';
+        var asideVisible = parseInt($('aside-menu').getStyle('margin-left')) === 0;
+
+        if (false === asideButtonVisible && false === asideVisible) {
+            showAside();
+        }
+
+        if (true === asideButtonVisible && true === asideVisible) {
+            hideAside();
+        }
+    });
+}
--- http/assets/js/header-menu.js
+++ http/assets/js/header-menu.js
@@ -1,32 +1,32 @@
 'use strict';
 
+if ($('header-menu-button')) {
+    /**
+     * Open and close button for menu
+     */
+    $('header-menu-button').addEvent('click', function (event) {
+        $('header-menu').setStyle(
+            'display',
+            $('header-menu').getStyle('display') === 'none' ? 'block' : 'none'
+        );
+    });
 
 
-/**
- * Open and close button for menu
- */
-$('header-menu-button').addEvent('click', function (event) {
-    $('header-menu').setStyle(
-        'display',
-        $('header-menu').getStyle('display') === 'none' ? 'block' : 'none'
-    );
-});
 
+    /**
+     * Catch all clicks on the html element (everything basicly), then filter out
+     * which is not interesting (menu button and menu itself) and then close the
+     * menu if it's not catched in the not interesting filter
+     */
+    $$('html')[0].addEvent('click', function (event) {
+        if (event.target                           === $('header-menu-button') ||
+            event.target.getParent('#header-menu') === $('header-menu') ||
+            event.target                           === $('header-menu')) {
+            return false;
+        }
 
-
-/**
- * Catch all clicks on the html element (everything basicly), then filter out
- * which is not interesting (menu button and menu itself) and then close the
- * menu if it's not catched in the not interesting filter
- */
-$$('html')[0].addEvent('click', function (event) {
-    if (event.target                           === $('header-menu-button') ||
-        event.target.getParent('#header-menu') === $('header-menu') ||
-        event.target                           === $('header-menu')) {
-        return false;
-    }
-
-    if ('block' === $('header-menu').getStyle('display')) {
-        $('header-menu').setStyle('display', 'none');
-    }
-});
+        if ('block' === $('header-menu').getStyle('display')) {
+            $('header-menu').setStyle('display', 'none');
+        }
+    });
+}
