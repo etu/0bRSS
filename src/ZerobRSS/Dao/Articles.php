8092f9aab4000ec7176ad8126c2844d5af085dc0
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-12 16:48:21 +0200

    Added Articles Dao

---
+++ src/ZerobRSS/Dao/Articles.php
@@ -0,0 +1,48 @@
+<?php
+namespace ZerobRSS\Dao;
+
+use \Doctrine\DBAL\Connection as Db;
+
+class Articles
+{
+    /** @var Db */
+    private $db;
+
+    public function __construct(Db $db)
+    {
+        $this->db = $db;
+    }
+
+    public function create($values)
+    {
+        $query = $this->db->createQueryBuilder()
+               ->insert('articles');
+
+        // Append parameters to insert to the query
+        foreach ($values as $key => $value) {
+            $query = $query->setValue($key, ':'.$key)->setParameter(':'.$key, $value);
+        }
+
+        $query->execute();
+
+        // @TODO: Check if this works in MariaDB
+        return $this->db->lastInsertId('articles_id_seq');
+    }
+
+    // Update article by unique identifier
+    public function update($identifier, $values)
+    {
+        $query = $this->db->createQueryBuilder()
+               ->update('articles')
+               ->where('identifier = :id AND feed_id = :feedid')
+               ->setParameter(':id',     $values['identifier'])
+               ->setParameter(':feedid', $values['feed_id']);
+
+        // Append parameters to insert to the query
+        foreach ($values as $key => $value) {
+            $query = $query->set($key, ':'.$key)->setParameter(':'.$key, $value);
+        }
+
+        return $query->execute();
+    }
+}
