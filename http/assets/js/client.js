5a0e8e79b6cf1aea04cf05495fd70a65b7b8401c
Elis Axelsson <elis.axelsson@gmail.com>
2015-07-15 19:40:00 +0200

    Added Handlebars and template plus script to fetch feeds from API and draw the menu with feeds

---
+++ http/assets/js/client.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+/**
+ * Class to fetch the feed list and draw the aside
+ */
+var Feeds = new Class({
+    feeds: [],
+    initialize: function () {
+        this.loadFeeds();
+    },
+
+    /** Load feeds from API and trigger the drawing function */
+    loadFeeds: function () {
+        new Request.JSON({
+            method: 'get',
+            url: window.ZerobRSS.apiUri + '/v1/feeds',
+            onComplete: function (response) {
+                window.ZerobRSS.Feeds.feeds = response;
+
+                window.ZerobRSS.Feeds.drawAside();
+            }
+        }).send();
+    },
+
+    drawAside: function () {
+        var template = Handlebars.compile($('feed-menu-template').get('html'));
+
+        this.feeds.each(function(feed) {
+            console.log(feed);
+            var a = new Element('a');
+
+            a.set('href', '#!/feeds/' + feed.id);
+            a.set('html', template(feed));
+
+            a.inject($$('#aside-menu nav')[0]);
+        });
+    }
+});
+
+window.ZerobRSS.Feeds = new Feeds();
