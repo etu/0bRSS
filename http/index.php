a9497b8da0c9b1a96fc0073d140c760ac3542cb2
Elis Axelsson <elis.axelsson@gmail.com>
2015-11-16 19:48:03 +0100

    Require php 5.5

--- http/index.php
+++ http/index.php
@@ -41,7 +41,7 @@ $slim->view->parserExtensions[] = new \Twig_Extension_Debug();
 /**
  * Load the Middleware class with clasures for loading controllers and middlewares
  */
-$mws = $injector->make('\ZerobRSS\Middlewares');
+$mws = $injector->make(\ZerobRSS\Middlewares::class);
 
 
 
