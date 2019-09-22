365c1daf78ae00ee39e9ef4e5efdc3ded98d5baf
Elis Hirwing <elis@hirwing.se>
2019-09-22 20:27:56 +0200

    WIP: Fix JS Controller

--- app/src/ZerobRSS/Controllers/Js.php
+++ app/src/ZerobRSS/Controllers/Js.php
@@ -1,24 +1,42 @@
 <?php
+declare(strict_types=1);
+
 namespace ZerobRSS\Controllers;
 
-use \Slim\Slim;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Psr7\Factory\StreamFactory;
+use ZerobRSS\Config;
 
 class Js
 {
-    /** @var Slim */
-    private $silm;
+    /** @var Config */
+    private $config;
 
-    public function __construct(Slim $slim)
+    /** @var StreamFactory */
+    private $streamFactory;
+
+    public function __construct(Config $config, StreamFactory $streamFactory)
     {
-        $this->slim = $slim;
+        $this->config = $config;
+        $this->streamFactory = $streamFactory;
     }
 
-    public function get($jsFile)
+    public function __invoke(Request $request, Response $response, array $args = []) : Response
     {
-        $jsPath = PROJECT_ROOT.'/http/assets/js';
-        $jsFile = $jsPath.'/'.$jsFile;
+        // Find file paths
+        $jsPath = $this->config->projectRoot.'/public/assets/js';
+        $jsFile = $jsPath.'/'.$args['file'];
 
-        $this->slim->response->headers->set('Content-Type', 'application/javascript');
-        echo file_get_contents($jsFile);
+        // Return 404 if file isn't found
+        if (!file_exists($jsFile)) {
+            return $response->withStatus(404);
+        }
+
+        // Set header
+        $response = $response->withHeader('Content-Type', 'application/javascript');
+
+        // Return data
+        return $response->withBody($this->streamFactory->createStreamFromFile($jsFile));
     }
 }
