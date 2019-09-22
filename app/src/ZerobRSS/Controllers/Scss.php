f5ddb7f7954863976e21d085a5bfa2b3c8629985
Elis Hirwing <elis@hirwing.se>
2019-09-22 20:16:39 +0200

    WIP: Fix Scss controller

--- app/src/ZerobRSS/Controllers/Scss.php
+++ app/src/ZerobRSS/Controllers/Scss.php
@@ -1,42 +1,48 @@
 <?php
+declare(strict_types=1);
+
 namespace ZerobRSS\Controllers;
 
-use \Slim\Slim;
-use \Leafo\ScssPhp\Compiler;
+use Leafo\ScssPhp\Compiler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Psr7\Factory\StreamFactory;
+use ZerobRSS\Config;
 
 class Scss
 {
-    /**
-     * @var Compiler
-     */
+    /** @var Config */
+    private $config;
+
+    /** @var Compiler */
     private $compiler;
 
-    /**
-     * @var Slim
-     */
-    private $slim;
+    /** @var StreamFactory */
+    private $streamFactory;
 
-    public function __construct(Compiler $compiler, Slim $slim)
+    public function __construct(Config $config, Compiler $compiler, StreamFactory $streamFactory)
     {
+        $this->config = $config;
         $this->compiler = $compiler;
-        $this->slim  = $slim;
+        $this->streamFactory = $streamFactory;
     }
 
-    public function get($cssFile)
+    public function __invoke(Request $request, Response $response, array $args = []) : Response
     {
-        $scssPath = PROJECT_ROOT.'/src/scss';
-        $scssFile = $scssPath.'/'.str_replace('css', 'scss', $cssFile);
+        $scssPath = $this->config->projectRoot.'/src/scss';
+        $scssFile = $scssPath.'/'.str_replace('css', 'scss', $args['file']);
 
-
-        /** Pass the route if file don't exists, will result in 404 */
+        // Return 404 if file isn't found
         if (!file_exists($scssFile)) {
-            return $this->slim->pass();
+            return $response->withStatus(404);
         }
 
+        $response = $response->withHeader('Content-Type', 'text/css');
 
-        $this->slim->response->headers->set('Content-Type', 'text/css');
-
+        // Set up compiler
         $this->compiler->addImportPath($scssPath);
-        echo $this->compiler->compile(file_get_contents($scssFile));
+        $css = $this->compiler->compile(file_get_contents($scssFile));
+
+        return $response->withBody($this->streamFactory->createStream($css));
     }
 }
