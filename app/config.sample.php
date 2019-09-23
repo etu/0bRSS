5af8a22e01376f2b2ddadc67e9343a92807e6bf7
Elis Hirwing <elis@hirwing.se>
2019-09-23 19:31:28 +0200

    WIP: Revert config file format for phinx migrations to work

--- app/config.sample.php
+++ app/config.sample.php
@@ -1,16 +1,25 @@
 <?php
 
 return [
-    'debug' => false,               // Display debug data
-    'bcrypt-password-cost' => 10,   // Bcrypt password cost
+    'debug' => false,                   // Display debug data
+    'bcrypt-password-cost' => 10,       // bcrypt password cost
 
-    'database' => [
-        'adapter' => 'pgsql',       // pgsql, mysql
-        'host'    => 'localhost',   // Database hostname
-        'name'    => '0bRSS',       // Database name
-        'user'    => '0bRSS',       // Database username
-        'pass'    => 'secret-pass', // Database password
-        'port'    => 5432,          // Database port: 5432 for pgsql and 3306 for mysql
-        'charset' => 'utf8',        // Charset, don't change this
+    'paths' => [
+        'migrations' => __DIR__.'/migrations',
+    ],
+
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database' => 'database',
+
+        'database' => [
+            'adapter' => 'pgsql',       // pgsql, mysql
+            'host'    => 'localhost',   // Database hostname
+            'name'    => '0bRSS',       // Database name
+            'user'    => '0bRSS',       // Database username
+            'pass'    => 'secret-pass', // Database password
+            'port'    => 5432,          // Database port: 5432 for pgsql and 3306 for mysql
+            'charset' => 'utf8',        // Charset, don't change this
+        ],
     ],
 ];
