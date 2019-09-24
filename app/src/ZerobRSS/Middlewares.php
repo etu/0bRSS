eae3659173613e05d58af911448778702d750b28
Elis Hirwing <elis@hirwing.se>
2019-09-24 17:32:32 +0200

    WIP: Clean out middlewares to load controllers and databases

--- app/src/ZerobRSS/Middlewares.php
+++ app/src/ZerobRSS/Middlewares.php
@@ -3,7 +3,6 @@ namespace ZerobRSS;
 
 use \Auryn\Injector;
 use \Slim\Slim;
-use \Doctrine\DBAL\Configuration as DbalConfig;
 
 class Middlewares
 {
@@ -16,59 +15,11 @@ class Middlewares
     /** @var DbalConfig */
     private $dbalConfig;
 
-    public function __construct(Injector $injector, Slim $slim, DbalConfig $dbalConfig)
+    public function __construct(Injector $injector, Slim $slim)
     {
         $this->injector = $injector;
         $this->slim = $slim;
-        $this->dbalConfig = $dbalConfig;
     }
-
-
-
-    /**
-     * Closure to load controllers
-     */
-    public function controllerLoader($controller, $method)
-    {
-        $injector = $this->injector;
-
-        return function () use ($controller, $method, $injector) {
-            $controller = $injector->make('\ZerobRSS\Controllers\\'.$controller);
-
-            return call_user_func_array([$controller, $method], func_get_args());
-        };
-    }
-
-
-
-    /**
-     * Middleware to initiate the database connection
-     */
-    public function db()
-    {
-        $injector = $this->injector;
-
-        return function () use ($injector) {
-            $config = require(PROJECT_ROOT.'/config.php');
-            $dbConfig = $config['environments']['database'];
-
-
-            // Connect to database
-            $dbalConn = \Doctrine\DBAL\DriverManager::getConnection([
-                'url' => $dbConfig['adapter'].'://'.$dbConfig['user'].':'.$dbConfig['pass'].'@'.$dbConfig['host'].':'
-                        .$dbConfig['port'].'/'.$dbConfig['name'].'?charset='.$dbConfig['charset']
-            ], $this->dbalConfig);
-
-
-            // Set default fetch-mode to fetch objects
-            $dbalConn->setFetchMode(\PDO::FETCH_OBJ);
-
-
-            // Share \Doctrine\DBAL\Connection
-            $injector->share($dbalConn);
-        };
-    }
-
 
 
     /**
