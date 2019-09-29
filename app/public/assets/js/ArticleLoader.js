2eeca261db9e0ad41421d2393fa3b3c92f1f5dec
Elis Hirwing <elis@hirwing.se>
2019-09-29 17:34:42 +0200

    WIP: Inital work to rewrite the article loader

--- app/public/assets/js/ArticleLoader.js
+++ app/public/assets/js/ArticleLoader.js
@@ -1,85 +1,49 @@
 'use strict';
 
-var ArticleLoader = new Class({
-    feed: 12,
-    page: 0,
+class ArticleLoader {
+    constructor() {
+    }
 
-    initialize: function (feed) {
-        this.feed = feed;
+    async loadArticles(feedId, unread) {
+        // Build API URI
+        var uri = window.ZerobRSS.apiUri + '/v1/feeds/' + feedId + '/articles?token=' + window.ZerobRSS.Auth.token;
 
-        new Request.JSON({
-            method: 'get',
-            url: window.ZerobRSS.apiUri + '/v1/feeds/' + this.feed,
-            onComplete: function (response) {
-                window.ZerobRSS.ArticleLoader.unread = response.unread;
-
-                window.ZerobRSS.ArticleLoader.getArticles();
-            }
-        }).send();
-    },
-
-    getArticles: function () {
-        var readGet = null;
-
-        if (this.unread > 0) {
-            readGet = '&read=false';
+        if (unread > 0) {
+            uri += '&read=false';
         }
 
-
-
-        new Request.JSON({
-            method: 'get',
-            url: window.ZerobRSS.apiUri + '/v1/feeds/' + this.feed + '/articles?page=' + this.page + readGet,
-            onComplete: function (response) {
-                var template = Handlebars.compile($('news-card-template').get('html'));
-
-                response.each(function (article) {
-                    var a = new Element('article');
-
-                    a.set('data-id', article.identifier);
-                    a.set('data-feed-id', article.feed_id);
-                    a.set('html', template(article));
-
-                    if (article.read) {
-                        a.addClass('read');
-                    }
-
-                    a.addEvent('click', (function (e) {
-                        window.ZerobRSS.ArticleLoader.clickArticle(e.target.getParent('article').get('data-id'));
-                    }));
-
-                    a.inject($('content'));
-                });
-            }
-        }).send();
-    },
-
-    clickArticle: function (id) {
-        window.ZerobRSS.Keyboard.currentArticle = id;
-
-        $$('#content > article').each(function (elem) {
-            elem.removeClass('active');
+        // Do request
+        var response = await fetch(uri, {
+            headers: {
+                'Accept': 'application/json',
+            },
         });
 
-        var article = $$('article[data-id=' + id + ']')[0];
+        // Parse response
+        var articles = await response.json();
 
-        article.addClass('active');
+        var template = `
+            ${articles.map(article => `
+              <article>
+                  <header>
+                      ${article.title}
+                      <span class="extras">
+                          <a href="${article.uri}" target="_blank">&#x1f517;</a>
+                          <i>&#x2605; &#x2606;</i>
+                      </span>
+                  </header>
+                  <div class="article-body">
+                      ${article.body}
+                  </div>
+                  <footer>
+                     ${article.date}
+                  </footer>
+              </article>
+            `)}
+        `;
 
-        new Request.JSON({
-            url: window.ZerobRSS.apiUri + '/v1/articles/' + id,
-            data: JSON.encode({'read': true}),
-            emulation: false,
-            onComplete: function (response) {
-                var wasRead = article.hasClass('read');
-
-                article.addClass('read');
-
-                if (!wasRead) {
-                    var read = $$('#aside-menu > nav > a[data-feed-id=' + article.get('data-feed-id') + '] .unread')[0];
-
-                    read.set('html', read.get('html') - 1);
-                }
-            }
-        }).put();
+        document.getElementById('content').innerHTML += template;
     }
-});
+}
+
+window.ZerobRSS.ArticleLoader = new ArticleLoader();
